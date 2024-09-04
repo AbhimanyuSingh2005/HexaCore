810d94f5c8736215e7558b02951599c4580639a0
Abhimanyu Singh <abhimanyusik@gmail.com>
2024-09-04 11:03:47 +0530

    connected mongoDB

---
+++ app.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+
+require("./db/conn");
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
